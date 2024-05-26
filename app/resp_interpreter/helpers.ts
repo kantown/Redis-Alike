e1cafd1c88d3bb3fe95eb546042c287bef6b30cd
Bartosz Kawa <ghostpand@gmail.com>
2024-05-26 14:11:07 +0200

    remove eol

--- app/resp_interpreter/helpers.ts
+++ app/resp_interpreter/helpers.ts
@@ -2,9 +2,9 @@ import { EOL } from "os";
 import { FIRST_BYTES_CODES } from "./types";
 
 export const toSimpleString = (message: string) => {
-  return `${FIRST_BYTES_CODES.SIMPLE_STRING}${message}${EOL}`;
+  return `${FIRST_BYTES_CODES.SIMPLE_STRING}${message}`;
 };
 
 export const toSimpleError = (message: string) => {
-  return `${FIRST_BYTES_CODES.SIMPLE_STRING}${message}${EOL}`;
+  return `${FIRST_BYTES_CODES.SIMPLE_STRING}${message}`;
 };
